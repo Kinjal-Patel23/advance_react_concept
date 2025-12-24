12d85a9c6a901ac745d2a3a9855db88f5ad8b469
Kinjal-Patel23 <kkaneriya11@gmail.com>
2025-12-24 14:01:17 +0530

    task_2

---
+++ task2/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Counter1 from './components/Counter1'
+import Counter2 from './components/Counter2'
+import Counter3 from './components/Counter3'
+
+const App = () => {
+  return (
+    <>
+      <Counter1/>
+      <Counter2/>
+      <Counter3/>
+    </>
+  )
+}
+
+export default App
