b6966824fcd2d65a6184f4f711cb9cf2594a537d
Kinjal Kaneriya <kkaneriya11@gmail.com>
2025-12-29 18:03:10 +0530

    update Task-2

--- task2/src/components/Counter1.jsx
+++ task2/src/components/Counter1.jsx
@@ -14,39 +14,39 @@ const Counter1 = () => {
   return (
     <>
       <div className="w-64 p-6 rounded-2xl bg-linear-to-br from-blue-700 to-green-600 text-center shadow-xl">
-  
-  <h1 className="text-5xl font-bold text-white mb-6">
-    {count}
-  </h1>
 
-  <div className="flex gap-3">
-    
-    <button
-      onClick={decrement}
-      className="flex-1 py-2 rounded-xl bg-red-500 text-white text-xl font-bold
+        <h1 className="text-5xl font-bold text-white mb-6">
+          {count}
+        </h1>
+
+        <div className="flex gap-3">
+
+          <button
+            onClick={decrement}
+            className="flex-1 py-2 rounded-xl bg-red-500 text-white text-xl font-bold
                  hover:bg-red-600 active:scale-95 transition"
-    >
-      −
-    </button>
+          >
+            −
+          </button>
 
-    <button
-      onClick={reset}
-      className="flex-1 py-2 rounded-xl bg-yellow-400 text-black font-semibold
+          <button
+            onClick={reset}
+            className="flex-1 py-2 rounded-xl bg-yellow-400 text-black font-semibold
                  hover:bg-yellow-500 active:scale-95 transition"
-    >
-      Reset
-    </button>
+          >
+            Reset
+          </button>
 
-    <button
-      onClick={handleIncrement}
-      className="flex-1 py-2 rounded-xl bg-green-500 text-white text-xl font-bold
+          <button
+            onClick={handleIncrement}
+            className="flex-1 py-2 rounded-xl bg-green-500 text-white text-xl font-bold
                  hover:bg-green-600 active:scale-95 transition"
-    >
-      +
-    </button>
+          >
+            +
+          </button>
 
-  </div>
-</div>
+        </div>
+      </div>
 
 
     </>
