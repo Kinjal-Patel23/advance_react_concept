aee443e52668b67deeeb1e107c80066d8f9efa8b
kinjal-kaneriya <kinjalk.encircle@gmail.com>
2025-12-30 12:58:34 +0530

    Complete Task 5

--- task5/src/Header.jsx
+++ task5/src/Header.jsx
@@ -1,24 +1,23 @@
 import React, { useEffect } from "react";
 
 const Header = React.memo(({ search, onSearch }) => {
+  useEffect(() => {
+    console.log("Header re-rendered");
+  });
 
-    useEffect(() => {
-        console.log("Header re-rendered");
-    });
+  return (
+    <div className="mb-6">
+      <h1 className="text-2xl font-bold mb-3">Posts Search</h1>
 
-    return (
-        <div className="mb-6">
-            <h1 className="text-2xl font-bold mb-3">Posts Search</h1>
-
-            <input
-                type="text"
-                value={search}
-                onChange={(e) => onSearch(e.target.value)}
-                placeholder="Search posts..."
-                className="w-full p-3 rounded-lg bg-zinc-900 border border-zinc-700 text-white"
-            />
-        </div>
-    );
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => onSearch(e.target.value)}
+        placeholder="Search posts..."
+        className="w-full p-3 rounded-lg bg-zinc-900 border border-zinc-700 text-white"
+      />
+    </div>
+  );
 });
 
 export default Header;
--- task5/src/PostList.jsx
+++ task5/src/PostList.jsx
@@ -2,25 +2,24 @@ import { useMemo, useEffect } from "react";
 import PostItem from "./PostItem";
 
 const PostList = ({ posts, search }) => {
+  useEffect(() => {
+    console.log("PostList re-rendered");
+  });
 
-    useEffect(() => {
-        console.log("PostList re-rendered");
-    });
-
-    const filteredPosts = useMemo(() => {
-        console.log("Filtering posts...");
-        return posts.filter((post) =>
-            post.title.toLowerCase().includes(search.toLowerCase())
-        );
-    }, [posts, search]);
-
-    return (
-        <div className="space-y-4">
-            {filteredPosts.map((post) => (
-                <PostItem key={post.id} post={post} />
-            ))}
-        </div>
+  const filteredPosts = useMemo(() => {
+    console.log("Filtering posts...");
+    return posts.filter((post) =>
+      post.title.toLowerCase().includes(search.toLowerCase()),
     );
+  }, [posts, search]);
+
+  return (
+    <div className="space-y-4">
+      {filteredPosts.map((post) => (
+        <PostItem key={post.id} post={post} />
+      ))}
+    </div>
+  );
 };
 
 export default PostList;
